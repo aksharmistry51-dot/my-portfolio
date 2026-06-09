7df0265cb917cf7e377087312ffc51f020f9b746
aksharmistry51-dot <aksharmistry51@gmail.com>
2026-06-09 07:41:47 +0530

    fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
-<div className="bg-slate-900 min-h-screen text-white">
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Skills from "./components/Skills"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+
+export default function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen text-white">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
